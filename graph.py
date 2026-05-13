3ea57f6886dfa2a178c22e38a110b8e7e06a39ce
Yash Pardeshi <ashpardeshi07@gmail.com>
2026-05-14 03:56:31 +0530

    Phase 1: LangGraph interrupt and checkpoint mechanics

--- graph.py
+++ graph.py
@@ -1,5 +1,8 @@
 from typing import TypedDict
-from langgraph.types import interrupt
+from langgraph.types import interrupt, Command
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 class MathState(TypedDict):
     input_number: float
@@ -27,3 +30,37 @@ def Divide_node(state: MathState):
 def Add_node(state: MathState):
     new_number = state["input_number"] + 10
     return {"input_number": new_number}
+
+def should_continue(state: MathState):
+    if state["human_decision"] == "yes":
+        return "add"
+    else:
+        return END
+
+
+conn = sqlite3.connect("checkpoints.sqlite", check_same_thread=False)
+memory = SqliteSaver(conn)
+
+builder = StateGraph(MathState)
+builder.add_node("multiply", Multiply_node)
+builder.add_node("divide", Divide_node)
+builder.add_node("add", Add_node)
+builder.add_edge(START, "multiply")
+builder.add_edge("multiply", "divide")
+builder.add_conditional_edges("divide", should_continue)
+builder.add_edge("add", END)
+graph = builder.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "1"}}
+# print("--- Starting Graph ---")
+# initial_input = {"input_number": 10}
+# result = graph.invoke(initial_input, config)
+# print(result)
+
+
+print("--- Resuming Graph ---")
+try:
+    result = graph.invoke(Command(resume="yes"), config)
+    print(result)
+except Exception as e:
+    print(f"Error: {e}")
